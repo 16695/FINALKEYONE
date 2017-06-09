7fe0991052ad908fd58fadce119ad80c722091a3
DESKTOP-POV94G3\Neftali <13940005@itstacambaro.edu.mx>
2017-06-09 17:44:19 -0500

    club de Danza

---
+++ danza.php
@@ -0,0 +1,88 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style type="text/css">
+<!--
+body{background: url(Fondos/background-5.jpg) no-repeat center;
+     background-size:1350px 1350px
+	}
+->
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<script src="includes/ice/ice.js" type="text/javascript"></script>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+</head>
+
+
+
+
+<body>
+
+<center>
+  <p><img src="imagen/top.png" width="1200" height="150" alt="k" /></p>
+  <table>
+  <td><td><td><td><td><td>
+  <td align="center">
+  <ul id="MenuBar1" class="MenuBarHorizontal">
+    <li><a href="menuadmin.php">INICIO</a></li>
+    <li><a class="MenuBarItemSubmenu" href="#">REGISTRO</a>
+      <ul>
+        <li><a href="#" class="MenuBarItemSubmenu">NUEVO</a>
+          <ul>
+            <li><a href="#">ALUMNO</a></li>
+            <li><a href="#">CLUB</a></li>
+            <li><a href="#">EVENTO</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="MenuBarItemSubmenu">EDITAR</a>
+          <ul>
+            <li><a href="#">ALUMNO</a></li>
+            <li><a href="#">CLUB</a></li>
+            <li><a href="#">EVENTO</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="MenuBarItemSubmenu">ELIMINAR </a>
+          <ul>
+            <li><a href="#">ALUMNO</a></li>
+            <li><a href="#">CLUB</a></li>
+            <li><a href="#">EVENTO</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a href="#" class="MenuBarItemSubmenu">CLUBS</a>
+      <ul>
+        <li><a href="#">BANDA</a></li>
+        <li><a href="#">DANZA</a></li>
+        <li><a href="#">PINTURA</a></li>
+        <li><a href="#">FUTBOL</a></li>
+        <li><a href="#">ROBOTICA</a></li>
+        <li><a href="#">BASQUET</a></li>
+      </ul>
+    </li>
+    <li><a href="#">BUSQUEDA</a></li>
+    <li><a href="#">NOTIFICACION</a></li>
+    <li><a href="#">SALIDAS</a></li>
+    <li><a href="#">EXPORTAR</a></li>
+    <li><a href="#">IMPRIMIR</a></li>
+    <li><a href="#">CERRAR SESION</a></li>
+  </ul>
+  </td> </td> </td> </td> </td> </td>
+  </table>
+</center>
+<div class="col-lg-5"   style="background-color: #C60; height:35px; width:1200px; margin-right:auto; margin-left:auto;   "></div>
+<div class="col-lg-5"   style="background-color: #FFF; height:500px; width:1200px; margin-right:auto; margin-left:auto;   ">
+
+<div align="right"></div>
+
+</div>
+<center>
+<img src="imagen/Captura.PNG" width="1200" height="75" alt="j" />
+</center>
+<script type="text/javascript">
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+</script>
+</body>
+</html>
