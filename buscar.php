778b7d97dc5c5410ccbc8b3ad2b5dcd2d9f577d2
DESKTOP-POV94G3\Neftali <13940005@itstacambaro.edu.mx>
2017-06-09 17:43:04 -0500

    Club de banda de guerra

---
+++ buscar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<script src="includes/ice/ice.js" type="text/javascript"></script>
+</head>
+
+
+
+
+<body background="imagen/240_F_70655085_kAsIx4P17hhaD1aYyg9C13L0Q95ml0NM.jpg">
+<center>
+  <p><img src="imagen/top.png" width="1200" height="150" alt="k" /></p>
+ 
+</center>
+<div class="col-lg-5"   style="background-color: #C60; height:35px; width:1200px; margin-right:auto; margin-left:auto;   "></div>
+<div class="col-lg-5"   style="background-color: #FFF; height:500px; width:1200px; margin-right:auto; margin-left:auto;   ">
+
+<div align="right">
+                <input type="button" value="regresar" onClick="history.back(-1)" />
+              </div>
+<table align='left' width='23%' border='0' cellspacing='3' cellpadding='1'>
+			    <tr>
+<center>
+<th bgcolor= "#66FFCC">NUMERO DE CONTROL</th>
+</center>
+</tr>
+<tr>
+
+<center>
+<td><input type="text" name="NUMERO" ></td> 
+</center>
+</tr> 
+</table>
+			
+
+</div>
+<center>
+<img src="imagen/Captura.PNG" width="1200" height="75" alt="j" />
+</center>
+</body>
+</html>
