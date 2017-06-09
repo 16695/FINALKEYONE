465f26fa6676670c4d9dfd8c8eadacbf84a9bc60
16695 <13940005@itstacambaro.edu.mx>
2017-06-09 18:00:36 -0500

    Pagina de Inicio

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+</head>
+
+<body bgcolor="#33FF99"
+<p>
+<center>
+  <label for="textfield"></label>
+
+    <img src="imagen/top.png" width="960" height="150" /></p>    
+<p>
+</center>
+<center>
+  
+ <MARQUEE BEHAVIOR=alternate>   <h1>INICIO DE SESION</h1 >  </p>    </MARQUEE> 
+</center>
+<center>
+<p><img src="imagen/usuarios.png" width="204" height="178" /></p>
+
+</center>
+<center>
+  <form id="form1" name="form1" method="post" action="validar.php">
+<p>&nbsp;</p>
+  <p>usuario
+    <label for="textfield2"></label>
+  <input type="text" name="usuario" id="textfield2" />
+  </p>
+  <p>pass
+  <label for="textfield4"></label>
+    <input type="password" name="pass" id="password"/>
+  </p>
+  <p>
+    <input type="submit" name="button" id="button" value="Enviar" />
+    
+  </p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  
+  
+  
+ 
+  <p>&nbsp;</p>
+</form></center>
+<p>&nbsp;</p>
+
+</body>
+</html>
